8e5090ba44963dd0ae615b4d24093f6783c14e1d
Derekc142 <DErekc142@nycstudents.net>
2024-04-17 12:20:58 -0400

    w

--- app.py
+++ app.py
@@ -1,6 +1,47 @@
 import json
-## Open the JSON file of movie data
-movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
-data = json.load(movies)
+import os
 
+## Create Class for creating new dictionaries here
+with open("enemyinfo.json", "r") as f:
+
+
+  
+    # Serialize the updated Python list to a JSON string
+    data = json.load(f)
+class Enemies():
+    def __init__(self, name, atk, hp, skill):
+        self.name = name
+        self.atk = atk
+        self.hp = hp
+        self.skill = skill
+    ##Call classes in here
+
+while True:
+    N = input("Enter Enemy Name: ")
+    A = int(input("Enter Enemy ATK: "))
+    H = int(input("Enter Enemy HP: "))
+    S = input("Enter Enemy Skill: ")
+    enemies = Enemies(N,A,H,S)
+    data.append(enemies.__dict__)
+    new_enemy = input("Do you want to enter a new enemy? Y/N ")
+    if new_enemy.upper()== "N":
+        break
+
+
+
+
+
+
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(data)
+
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("data.json")
+os.rename(new_file, "data.json")
