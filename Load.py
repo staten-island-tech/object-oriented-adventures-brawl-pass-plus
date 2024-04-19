333ed93a7c9faa5828fedb3d364658cd3bf57df3
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-04-19 12:29:22 -0400

    Update Load.py

--- Load.py
+++ Load.py
@@ -1,2 +1,10 @@
 print("Welcome to Derek's Brainrot Simulator")
-print("Press Alt+F4 to start a new game")
+X = input("Type 'START' to start a new game and 'EXIT' to exit game ")
+if X == 'START':
+    Y = input("Start a new save file? (Y/N) ")
+while input:
+ if Y == 'Y':
+    print("Error")
+    X = ''
+if X == "EXIT":
+    print("KYS (Keep Yourself Safe)")
