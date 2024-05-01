3b7736eae3c376b367f293d411436ebce273d49c
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-05-01 12:00:45 -0400

    Update Load.py

--- Load.py
+++ Load.py
@@ -1,10 +1,10 @@
+Character_Name = []
+
 print("Welcome to Derek's Brainrot Simulator")
 X = input("Type 'START' to start a new game and 'EXIT' to exit game ")
 if X == 'START':
     Y = input("Start a new save file? (Y/N) ")
-while input:
- if Y == 'Y':
-    print("Error")
-    X = ''
-if X == "EXIT":
-    print("KYS (Keep Yourself Safe)")
+    if Y == 'Y':
+     Name = input("Input Charcter Name: ")
+     print("Confirm to name your character",Name,"?")
+     Character_Name.append(Name)
