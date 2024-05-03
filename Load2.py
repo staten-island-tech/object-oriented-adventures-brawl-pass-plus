a14733543c28177a530d0bc44db05ba97bb6b6eb
Derekc142 <DErekc142@nycstudents.net>
2024-05-03 12:10:12 -0400

    Update Load2.py

--- Load2.py
+++ Load2.py
@@ -50,7 +50,7 @@ if Begin == 'START':
     elif New_Save == 'N':
       F = input("Open A Previous Save? ")
       if F == 'Y':
-        print("Work in Progress")
+        print("")
       if F == 'N':
         print("Stop Trolling and Get a Life")
 
