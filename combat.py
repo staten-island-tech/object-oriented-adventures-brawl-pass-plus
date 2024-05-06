9750d3a2d4c9e65b7a1156b95913624b0acf0921
Derekc142 <DErekc142@nycstudents.net>
2024-05-06 12:28:23 -0400

    Update combat.py

--- combat.py
+++ combat.py
@@ -1,3 +1,13 @@
-1 attack 
-dfcsr hyjregruyol9k7u,ok-ymo-,o-re-rp;ep.eflpef
-'/f'fsfd. ,d
+import json
+enemies = open("enemyinfo.json", encoding="utf8")
+data = json.load(enemies)
+character = open("characterinfo.json", encoding="utf8")
+cdata = json.load(character)
+
+
+class combat():
+    print("1 attack 2 enhance attack 3 heal 4 retreat 5 exit game")
+    def attack(cdata):
+        if input == "1":
+            character ["ATK"] - enemies ["HP"]
+    def enhance_attack
