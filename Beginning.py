21fe8d948a26f5b4d5b86e42aa961901c963e07f
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-05-07 12:13:12 -0400

    Update Beginning.py

--- Beginning.py
+++ Beginning.py
@@ -1,35 +1,48 @@
-Main_Character = { 
-  'Character_Name': '',
-  'Faction': '',
-  'HP': '',
-  'ATK':  '',
-  'ENERGY': '',
-}
+Character_Name = []
+Faction = []
+HP = 0
+ATK = 0
+ENERGY = 0
+import json
+import os
 
+## Create Class for creating new dictionaries here
+with open("characterinfo.json", "r") as f:
+# Serialize the updated Python list to a JSON string
+ data = json.load(f)
+class main_character():
+    def __init__(self, Name, HP, ATK, ENERGY, Role):
+        self.Name = Name
+        self.HP = HP
+        self.ATK = ATK
+        self.ENERGY = ENERGY
+        self.Role = Role
 
 print("Welcome to Derek's Brainrot Simulator")
 Begin = input("Type 'START' to start a new game and 'EXIT' to exit game ")
 if Begin == 'START':
     New_Save = input("Start a new save file? (Y/N) ")
     if New_Save == 'Y':
-     Main_Character.clear
+     Character_Name.clear
      while input:
       Name = input("Input Charcter Name: ")
       print("Confirm to name your character", Name, "? (Y/N) ")
       Confirm = input("")
       if Confirm == 'Y':
-       Main_Character["Character_Name"] = [Name]
+       Character_Name.append(Name)
+       print(Character_Name)
        print("Warrior Stats: 120 HP, 20 ATK, 100 MAX ENERGY")
        print("Mage Stats: 80 HP, 10 ATK, 200 MAX ENERGY")
        print("Archer Stats: 100 HP, 15 ATK, 150 MAX ENERGY")
        print("Choose", Name,"'s role: ")
        Role = input("Warrior, Mage, or Archer? ")
-       Main_Character["Faction"] = [Role]
+       Faction.clear
+       Faction.append(Role)
        if Role == 'Warrior':
          HP = 120
          ATK = 20
          ENERGY = 100
-         print(Name,"is a", Role)
+         print(Name,"is an", Role)
          print("HP:", HP)
          print("ATK:", ATK)
          print("ENERGY:", ENERGY)
@@ -38,7 +51,7 @@ if Begin == 'START':
          HP = 80
          ATK = 10
          ENERGY = 200
-         print(Name,"is a", Role)
+         print(Name,"is an", Role)
          print("HP:", HP)
          print("ATK:", ATK)
          print("ENERGY:", ENERGY)
@@ -51,15 +64,37 @@ if Begin == 'START':
          print("HP:", HP)
          print("ATK:", ATK)
          print("ENERGY:", ENERGY)
+         main_character = main_character(Name, HP, ATK, ENERGY, Role)
+         data.append(main_character.__dict__)
          break
       if Confirm == 'N':
        Name = ' '
     elif New_Save == 'N':
       F = input("Open A Previous Save? ")
       if F == 'Y':
-        print(Main_Character)
+        print("")
       if F == 'N':
         print("Stop Trolling and Get a Life")
 if Begin == 'EXIT':
  print("Womp Womp")
  print("Exiting Game")
+
+class brawlers():
+ def __init__(self,Character_Name,rarity,role):
+        self.name = Character_Name
+        self.rarity = rarity
+        self.role = role
+
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(data)
+
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("characterinfo.json")
+os.rename(new_file, "characterinfo.json")
