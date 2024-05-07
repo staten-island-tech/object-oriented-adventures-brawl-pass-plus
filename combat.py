a6b27438b3d72fe27715f60904476b95e43b8c0a
Derekc142 <DErekc142@nycstudents.net>
2024-05-07 12:24:59 -0400

    Update combat.py

--- combat.py
+++ combat.py
@@ -38,6 +38,7 @@ class combat(battle):
 class enemycombat(battle):
     while battle:
         character["HP"] - enemies["ATK"]
+        print ("the enemy did ", enemies["ATK"], " dmg to you and you have ", character["HP"], "Hp remaining")
         if enemies["HP"] == 0:
             break
         print("Battle Won")
