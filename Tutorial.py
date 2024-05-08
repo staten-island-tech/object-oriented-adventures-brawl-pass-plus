1fc8aeab9aa40513b98496b5c195dbbd03b3cec4
Derekc142 <DErekc142@nycstudents.net>
2024-05-08 12:28:04 -0400

    Update Tutorial.py

--- Tutorial.py
+++ Tutorial.py
@@ -4,4 +4,54 @@ data = json.load(enemies)
 character = open("characterinfo.json", encoding="utf8")
 cdata = json.load(character)
 
-print("Welcome to the tutorial", "Name")
+class CharacterFinder():
+    def Search_CharacterName(cdata):
+        Z = input("Input Character Name: ")
+        for main_character in cdata:
+            if Z in main_character['Name'] :
+                print("Welcome to the tutorial", main_character["Name"])
+            else:
+                print("No data found for", Z)
+CharacterFinder.Search_CharacterName(cdata)
+
+class EnemyFinder():
+ def Search_EnemyName(data):
+  E = input("Input Enemy Name: ")
+  for basic_enemies in data:
+   if E in basic_enemies['Name'] :
+    print("Name:",basic_enemies['Name'])
+    print("HP:",basic_enemies['HP'])
+    print("ATK:",basic_enemies['ATK'])
+EnemyFinder.Search_EnemyName(data)
+
+class battle():
+    def __init__(self,combat, enemycombat):
+        self.combat = combat
+        self.enemycombat = enemycombat
+class combat(battle):
+    input("1 attack 2 enhance attack 3 heal 4 retreat 5 exit game")
+    def attack(cdata):
+        if input == "1":
+            enemies ["HP"] - character ["ATK"]
+    def enhance_attack(cdata):
+        if input == "2":
+            print ("how much energy do you want to use")
+            if input >= character ["ENERGY"]:
+                print ("you don't have enough energy")
+            else:
+                input + character ["ATK"]
+    def heal(cdata):
+        if input == "3":
+            print ("how much energy do you want to use")
+            if input >= character ["ENERGY"]:
+                print ("you don't have enough energy")
+            else:
+                input + character ["HP"]
+    def retreat():
+        if input == "4":
+            print ("retreating from battle")
+    def exit_game():
+        if input == "5":
+            print ("exiting game")
+
+
