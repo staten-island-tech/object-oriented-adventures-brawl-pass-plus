aa51a20e64a85c3f78272c9209f57e409cc50af9
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-05-08 11:55:38 -0400

    Update FindCharacter.py

--- FindCharacter.py
+++ FindCharacter.py
@@ -6,9 +6,15 @@ with open("characterinfo.json", "r") as f:
  data = json.load(main_characters)
 # create variable "data" that represents the enitre pokedex list
 
-def Search_CharacterName(data):
+class CharacterFinder():
+ def Search_CharacterName(data):
   Z = input("Input Character Name: ")
   for main_character in data:
    if Z in main_character['Name'] :
-    print(print(main_character['Name'], main_character['Role'], main_character['HP'], main_character['ATK'], main_character['ENERGY']))
+    print("Name:",main_character['Name'])
+    print("Role:",main_character['Role'])
+    print("HP:",main_character['HP'])
+    print("ATK:",main_character['ATK'])
+    print("ENERGY:",main_character['ENERGY'])
 
+CharacterFinder.Search_CharacterName(data)
