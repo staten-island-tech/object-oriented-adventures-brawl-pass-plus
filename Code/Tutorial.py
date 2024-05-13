6f8c3acec61ad1546628f2e5ffa1eae3423d93a0
Derekc142 <DErekc142@nycstudents.net>
2024-05-13 11:58:19 -0400

    Update Tutorial.py

--- Code/Tutorial.py
+++ Code/Tutorial.py
@@ -22,15 +22,8 @@ class EnemyFinder():
     print("Name:",basic_enemies['Name'])
     print("HP:",basic_enemies['HP'])
     print("ATK:",basic_enemies['ATK'])
-EnemyFinder.Search_EnemyName(data)
 
-
-
-class battle():
-    def __init__(self,combat, enemycombat):
-        self.combat = combat
-        self.enemycombat = enemycombat
-class combat(battle):
+class combat(EnemyFinder):
     def attack(cdata):
         enemies['HP'] - character['ATK']
     def enhance_attack(cdata):
@@ -49,6 +42,8 @@ class combat(battle):
             print ("retreating from battle")
     def exit_game():
             print ("exiting game")
+
+EnemyFinder.Search_EnemyName(data)
 
 print("1 attack 2 enhance attack 3 heal 4 retreat 5 exit game")
 x = input()
