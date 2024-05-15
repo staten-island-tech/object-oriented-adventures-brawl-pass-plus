fbeaf561d83a41dc907a067bb6942a2ef9b784f1
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-05-15 12:28:29 -0400

    1

--- Code/combat.py
+++ Code/combat.py
@@ -4,49 +4,16 @@ character = open("characterinfo.json", encoding="utf8")
 data = json.load(enemies)
 cdata = json.load(character)
 
-<<<<<<< HEAD
-class battle():
-    def __init__(self,combat, enemycombat):
-        self.combat = combat
-        self.enemycombat = enemycombat
-class combat(battle):
-    def attack(cdata):
-        if "Slime" in enemies['Name']:
-         HP = enemies['HP']
-         ATK = character['ATK']
-         print(HP - ATK)
-    def enhance_attack(cdata):
-            print ("how much energy do you want to use")
-            if input >= character ["ENERGY"]:
-                print ("you don't have enough energy")
-            else:
-                input + character ["ATK"]
-    def heal(cdata):
-            print ("how much energy do you want to use")
-            if input >= character ["ENERGY"]:
-                print ("you don't have enough energy")
-            else:
-                input + character ["HP"]
-    def retreat():
-            print ("retreating from battle")
-    def exit_game():
-            print ("exiting game")
-=======
-class CharacterFinder():
-    def Search_CharacterName(cdata):
-        Z = input("Input Character Name: ")
-        for main_character in cdata:
-            if Z in main_character['Name'] :
-                print("Welcome to the tutorial", main_character["Name"])
->>>>>>> origin/Derek
-
+Character = []
 
 class combat():
  def Search_CharacterName(cdata):
+    Character.clear
     Z = input("Input Character Name: ")
     for main_character in cdata:
      if Z in main_character['Name'] :
-      print("Welcome to the tutorial", main_character["Name"])
+      Character.append(main_character['Name'])
+      print("Welcome to the tutorial", Character)
  def Search_EnemyName(data):
   E = input("Input Enemy Name: ")
   for basic_enemies in data:
@@ -84,17 +51,16 @@ class combat():
  def exit_game():
     print ("exiting game")
 
-combat.Search_EnemyName(data)
-
+combat.Search_CharacterName(cdata)
 print("1 attack 2 enhance attack 3 heal 4 retreat 5 exit game")
 X = input()
-if X == '1':
+ if X == '1':
     combat.attack()
-elif X == '2':
+ elif X == '2':
     combat.enhance_attack()
-elif X == '3':
+ elif X == '3':
     combat.heal()
-elif X == '4':
+ elif X == '4':
     combat.retreat()
-elif X == '5':
+ elif X == '5':
     combat.exit_game()
