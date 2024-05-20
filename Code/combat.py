4af50492d202760484deb818dd3812403d9fdc6d
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-05-20 12:23:43 -0400

    1

--- Code/combat.py
+++ Code/combat.py
@@ -4,6 +4,7 @@ character = open("characterinfo.json", encoding="utf8")
 data = json.load(enemies)
 cdata = json.load(character)
 
+<<<<<<< HEAD
 Character = []
 Enemy = []
 Enemy_HP = []
@@ -12,26 +13,40 @@ Character_ATK = []
 Character_HP = []
 Input = []
 Input2 = []
+=======
+class CharacterFinder():
+    def Search_CharacterName(cdata):
+        Z = input("Input Character Name: ")
+        for main_character in cdata:
+            if Z in main_character['Name'] :
+                print("Welcome to the tutorial", main_character["Name"])
+
+>>>>>>> origin/Derek
 
 class combat():
  def Search_CharacterName(cdata):
     Z = input("Input Character Name: ")
     for main_character in cdata:
      if Z in main_character['Name'] :
+<<<<<<< HEAD
       Character.append(main_character['Name'])
       Character_Energy.append(main_character['ENERGY'])
       Character_ATK.append(main_character['ATK'])
       Character_HP.append(main_character['HP'])
+=======
+      print("Welcome to the tutorial", main_character["Name"])
+>>>>>>> origin/Derek
  def Search_EnemyName(data):
   E = input("Input Enemy Name: ")
   for basic_enemies in data:
    if E in basic_enemies['Name'] :
-    Enemy.append(basic_enemies['Name'])
-    Enemy_HP.append(basic_enemies['HP'])
-    print("Welcome to the tutorial", Character)
+    print("Name:",basic_enemies['Name'])
+    print("HP:",basic_enemies['HP'])
+    print("ATK:",basic_enemies['ATK']) 
+    basic_enemies = enemies
  def attack():
-    E = ''.join(Enemy)
-    Z = ''.join(Character)
+    E = input("Input Enemy Name: ")
+    Z = input("Input Character Name: ")
     for basic_enemies in data:
         for main_character in cdata:
             if Z in main_character['Name']:
@@ -39,36 +54,51 @@ class combat():
                     x = (basic_enemies["HP"])
                     y = (main_character["ATK"])
                     finalhp = x - y
-                    Enemy_HP.append(finalhp)
                     print (finalhp)
  def enhance_attack():
+<<<<<<< HEAD
     X = input("how much energy do you want to use? ")
     Y = int(X)
     Input.append(Y)
     if Input > Character_Energy:
         print ("you don't have enough energy!")
+=======
+    input ("how much energy do you want to use")
+    if input >= character ["ENERGY"]:
+        print ("you don't have enough energy")
+>>>>>>> origin/Derek
     else:
         Final_Energy = (Input + Character_Energy)
         Z = sum(Final_Energy)
         print("Your new attack is:", Z)
  def heal():
+<<<<<<< HEAD
     X = input("how much energy do you want to use? ")
     Y = int(X)
     Input2.append(Y)
     if Input2 > Character_Energy:
         print ("you don't have enough energy!")
+=======
+    print ("how much energy do you want to use")
+    if input >= character ["ENERGY"]:
+        print ("you don't have enough energy")
+>>>>>>> origin/Derek
     else:
         Final_HP = (Input2 + Character_HP)
         Z = sum(Final_HP)
         print("Your new HP is:", Z)
  def retreat():
-    print ("retreating from battle (imagine being a scaredy cat)")
+    print ("retreating from battle")
  def exit_game():
-    print ("exiting game...")
+    print ("exiting game")
 
-combat.Search_CharacterName(cdata)
 combat.Search_EnemyName(data)
+<<<<<<< HEAD
 print("1 = [attack], 2 = [enhance attack], 3 = [heal], 4 = [retreat], 5 = [exit game]")
+=======
+
+print("1 attack 2 enhance attack 3 heal 4 retreat 5 exit game")
+>>>>>>> origin/Derek
 X = input()
 if X == '1':
     combat.attack()
@@ -80,4 +110,3 @@ elif X == '4':
     combat.retreat()
 elif X == '5':
     combat.exit_game()
-
