781eb763d1fe851b639a1e95ad352435038b5917
Derekc142 <DErekc142@nycstudents.net>
2024-05-23 12:10:29 -0400

    e

--- Code/Skillpoints.py
+++ Code/Skillpoints.py
@@ -1,17 +1,22 @@
-availableskillpoints = []
 availableskillpoints = 1
-skillpoints_used = [""]
+skillpoints_used = []
 class skill_points():
     def skillpoints(HP, ATK, ENERGY):
-        HP = ("20")
-        ATK = ("10")
-        ENERGY = ("15")
-    X = input("how much skill points do you want to use? ")
-    X.append(skillpoints_used)
+        HP = 20
+        ATK = 10
+        ENERGY = 15
+    X = int(input("how much skill points do you want to use? "))
+    skillpoints_used.append(X)
     if skillpoints_used > availableskillpoints:
         print ("you don't have enough skill points!")
-    else:
-        F = input("Open A Previous Save? (Y/N)")
-        if F == 'Y':
+    else: 
+        S = input("Which stat do you want to improve? Hp, ATK, ENERGY")
+        if S == 'HP':
+            from Beginning import CharacterFinder
+            CharacterFinder.Search_CharacterName()
+        elif S == 'ATK':
+            from Beginning import CharacterFinder
+            CharacterFinder.Search_CharacterName()
+        elif S == 'ENERGY':
             from Beginning import CharacterFinder
             CharacterFinder.Search_CharacterName()
