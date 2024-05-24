4264044767d48a9e245ac3345b2b9360f7438166
Derekc142 <DErekc142@nycstudents.net>
2024-05-24 12:11:49 -0400

    e

--- Code/Beginning.py
+++ Code/Beginning.py
@@ -112,39 +112,8 @@ if Begin == 'START':
        F = input("Open A Previous Save? (Y/N) ")
        if F == 'Y':
         CharacterFinder.Search_CharacterName(data)
-availableskillpoints = ["1"]
-skillpoints_used = [""]
-class skill_point():
-      def skillpoints(HP, ATK, ENERGY):
-          HP = 20
-          ATK = 20
-          ENERGY = 15
-      X = input("how much skill points do you want to use? ")
-      skillpoints_used.append(X)
-      if skillpoints_used > availableskillpoints:
-          print ("you don't have enough skill points!")
-      else: 
-          S = input("Which stat do you want to improve? HP, ATK, ENERGY ")
-          if S == 'HP':
-            while():
-              CharacterFinder2.Search_CharacterName(data)
-              for main_character in data:
-                print("HP:",main_character['HP'])
-              break
-          elif S == 'ATK':
-            while():
-              CharacterFinder2.Search_CharacterName(data)
-              for main_character in data:
-                print("HP:",main_character['ATK'])
-          elif S == 'ENERGY':
-            while():
-              CharacterFinder2.Search_CharacterName(data)
-              for main_character in data:
-                print("HP:",main_character['ENERGY'])
-              break
-          else:
-            print("Stop Trolling and Get a Life")
-  break
+    else:
+      print("Stop Trolling and Get a Life")
 if Begin == 'EXIT':
  print("Womp Womp")
  print("Exiting Game")
