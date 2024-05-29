9def99adfd411431184f1052a2294c7c829abdfb
Derekc142 <DErekc142@nycstudents.net>
2024-05-29 12:26:27 -0400

    e

--- Code/combat.py
+++ Code/combat.py
@@ -41,8 +41,8 @@ class combat():
     Enemy.append(basic_enemies['Name'])
     Enemy_HP.append(basic_enemies['HP'])
     Enemy_ATK.append(basic_enemies['ATK'])
-    if (basic_enemies['ATK']) > (Character['HP']):
-      print ("You are not strong enough to fight this enemy")
+   if Enemy_ATK > Character_HP:
+      print ("You are not strong enough to fight this enemy, retreat if you want to survive as fighting the enemy is futile")
  def enemy_attack():
     x = int(''.join(map(str, Enemy_ATK)))
     y = int(''.join(map(str, Character_HP)))
