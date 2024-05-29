fcd7d4cc732a7a79045df5119ab54981b0062989
Derekc142 <DErekc142@nycstudents.net>
2024-05-29 12:08:53 -0400

    e

--- Code/combat.py
+++ Code/combat.py
@@ -113,6 +113,10 @@ class combat():
 
 class skill_point():
  def use_skillpoint():
+    print ("For every enemy you kill you will gain one skill point, each skill point will buff your")
+    print ("HP by 20")
+    print ("ATK by 20")
+    print ("ENERGY by 15")
     print("Skillpoints:", availableskillpoints)
     X = input("how much skill points do you want to use? ")
     Input3.clear()
--- Code/game.py
+++ Code/game.py
@@ -1,3 +1,2 @@
 from Beginning import CharacterFinder
 from Tutorial import tutorial
-from combat import combat, skill_point
