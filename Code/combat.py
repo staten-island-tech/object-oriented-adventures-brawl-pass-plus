ccded0dff50ce52dc40891aeef30c9d81da9030f
Derekc142 <DErekc142@nycstudents.net>
2024-05-29 12:07:36 -0400

    e

--- Code/combat.py
+++ Code/combat.py
@@ -114,10 +114,6 @@ class combat():
 class skill_point():
  def use_skillpoint():
     print("Skillpoints:", availableskillpoints)
-    print ("Each skill point will buff your")
-    print ("HP by 20")
-    print ("ATK by 20")
-    print ("ENERGY by 15")
     X = input("how much skill points do you want to use? ")
     Input3.clear()
     Input3.append(X)
